65df87a43497f1fa932d72ee788de8843c581f37
Sahiru22 <sahirubalasuriya222@gmail.com>
2024-08-22 01:08:47 +0530

    initial commit

---
+++ src/main/java/com/example/demo_spring_projection/DemoSpringProjectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_spring_projection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringProjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringProjectionApplication.class, args);
+	}
+
+}
